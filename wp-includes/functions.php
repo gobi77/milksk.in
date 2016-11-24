18f2998c150d59d977c1901c8e81cb1f01bbe1c4
gobi <gobibratuescu@gmail.com>
2016-11-24 23:49:19 +0200

    emptied everything from template

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -5464,3 +5464,13 @@ function wp_raise_memory_limit( $context = 'admin' ) {
 
 	return false;
 }
+
+// Include Bootstrap CDN
+function my_scripts_enqueue() {
+    wp_register_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), NULL, true );
+    wp_register_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', false, NULL, 'all' );
+
+    wp_enqueue_script( 'bootstrap-js' );
+    wp_enqueue_style( 'bootstrap-css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_scripts_enqueue' );
